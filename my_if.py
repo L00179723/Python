764c8487a03d553e54387b474c9ed6685b35e0f2
Suzanne Freitas <158096342+L00179723@users.noreply.github.com>
2024-01-29 21:47:49 +0000

    Add files via upload

---
+++ my_if.py
@@ -0,0 +1,17 @@
+a = True
+b = True
+c = True
+
+if a:
+    print("a was true")
+elif b:
+    print("b was true")
+elif c:
+    print("c was true")
+else:
+    print("None of our boolean variables were true")
+
+    if (3<2) and (5<9):
+    print("Yup!")
+else:
+    print("Nope!")
