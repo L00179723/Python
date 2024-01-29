145b55ece06501e0b72108d41a32008ced804faa
Suzanne Freitas <158096342+L00179723@users.noreply.github.com>
2024-01-29 21:47:05 +0000

    Add files via upload

---
+++ if_elif.py
@@ -0,0 +1,12 @@
+Python 3.12.0 (tags/v3.12.0:0fb18b0, Oct  2 2023, 13:03:39) [MSC v.1935 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> if condition1:
+...     # Execute code
+... elif condition2:
+...     # Execute code
+... else:
+...     # Execute other code
+...     
+SyntaxError: expected an indented block after 'if' statement on line 1
+>>> SyntaxError: expected an indented block after 'if' statement on line 1
+SyntaxError: invalid syntax
