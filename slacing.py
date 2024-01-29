1936aebb57fc5194aa5a95ee00e74116f16f5ab6
Suzanne Freitas <158096342+L00179723@users.noreply.github.com>
2024-01-29 21:46:17 +0000

    Add files via upload

---
+++ slacing.py
@@ -0,0 +1,4 @@
+a = 5
+b = 15
+print("Good afternoon, Suzanne. What would you like for lunch {}?".format("Food"))
+print ("We have {} eggs, {} bacon and a total of {} pieces available".format(a, b, a+b))
